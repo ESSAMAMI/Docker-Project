cd73e0e181389829a721d44b937528c2bfc49097
ESSAMAMI <hessamami@myges.fr>
2019-07-18 10:51:03 +0200

    Changes in app.py

--- app.py
+++ app.py
@@ -1,6 +1,28 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import pandas as pd
+from model.model import getConnection, getPerson, setPerson
+
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('crud.html')
+
+    person = getPerson()
+    if person:
+        return render_template('crud.html', person=person)
+    else:
+        return render_template('crud.html')
+
+@app.route('/add_consumer', methods=['POST'])
+def add_consumer():
+    pass
+
+    if request.method == 'POST':
+
+        name = request.form['name']
+        surname = request.form['surname']
+        address = request.form['address']
+        age = request.form['age']
+        setPerson(surname, name, age, address, 'SORRY NOT TODAY')
+        person = getPerson()
+        return render_template('crud.html', person=person)
